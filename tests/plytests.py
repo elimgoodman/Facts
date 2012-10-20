f7362141418d1afbb72a4b09a330b10f8e94c840
Eli Goodman <eli.m.goodman@gmail.com>
2012-10-20 10:42:16 -0400

    moar

--- tests/plytests.py
+++ tests/plytests.py
@@ -1,11 +1,17 @@
 import random
 import unittest
+import lang.plyer as plyer
 
 class TestPly(unittest.TestCase):
 
     def setUp(self):
-        self.seq = range(10)
+        pass
+    
+    def testFoo(self):
+        print(self.exec("$a = 1"))
 
-    #def exec
+    def exec(self, data):
+        plyer.lexer.input(data)
+        return plyer.parser.parse(data, lexer=plyer.lexer)
 if __name__ == '__main__':
     unittest.main()
